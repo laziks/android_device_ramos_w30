b2fd27913ad42a9de1fab4a6846c7fb288c0cc5e
laziks <usenet@thebat.net>
2013-01-28 12:39:07 +0100

    Update w30-blobs.mk

--- w30-blobs.mk
+++ w30-blobs.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := device/ramos/w30
+LOCAL_PATH := device/ramos/w30hd
     
 #Prebuilt Files
 PRODUCT_COPY_FILES += \
