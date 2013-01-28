c3d73bc5f9bdad92f1ad9d3f8244119985526764
laziks <usenet@thebat.net>
2013-01-28 12:37:42 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo full_w30-userdebug
-add_lunch_combo cm_w30-userdebug
+add_lunch_combo full_w30hd-userdebug
+add_lunch_combo cm_w30hd-userdebug
